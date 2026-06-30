120176180539e4735d1cf01400db469f6966e4b9
Ashford Reyes <abreyesreese@gmail.com>
2026-06-30 11:54:57 -0400

    Add file explorer button

--- labeler.py
+++ labeler.py
@@ -1,16 +1,15 @@
-from curses import window
 import tkinter as tk
-from tkinter import Button, Label, filedialog
+from tkinter import Label, filedialog, Button
 
 def browseFiles():
     filename = filedialog.askopenfilename(initialdir = "/",
                                           title = "Select a File",
-                                          filetypes = (("Text files",
-                                                        "*.txt*"),
-                                                       ("all files",
-                                                        "*.*")))
-     
-    # Change label contents
+                                            filetypes = (("Text files",
+                                                            "*.txt*"),
+                                                         ("all files",
+                                                            "*.*")))
+    
+
     label_file_explorer.configure(text="File Opened: "+filename)
 
 root = tk.Tk()
@@ -20,30 +19,23 @@ root.minsize(400, 300)
 root.configure(background="white")
 mycolor = '#f2f2f2'  # or use hex if you prefer 
 root.configure(bg=mycolor)
+textcolor = "#181A18"
 
-# Create a File Explorer label
-label_file_explorer = Label(window, 
+label_file_explorer = Label(root, 
                             text = "File Explorer using Tkinter",
                             width = 100, height = 4, 
-                            fg = "blue")
- 
-     
-button_explore = Button(window, 
+                            fg = textcolor)
+
+button_explore = Button(root, 
                         text = "Browse Files",
                         command = browseFiles) 
  
-button_exit = Button(window, 
+button_exit = Button(root, 
                      text = "Exit",
                      command = exit) 
- 
-# Grid method is chosen for placing
-# the widgets at respective positions 
-# in a table like structure by
-# specifying rows and columns
+
 label_file_explorer.grid(column = 1, row = 1)
- 
 button_explore.grid(column = 1, row = 2)
- 
 button_exit.grid(column = 1,row = 3)
 
 root.mainloop()
